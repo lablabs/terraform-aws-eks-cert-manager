0cedb2e5e378aeb9b940b6175d212539547472af
Ondřej Šmalec <ondrej.smalec@lablabs.io>
2021-08-26 14:53:20 +0200

    Removing unused providers
    Signed-off-by: Ondřej Šmalec <ondrej.smalec@lablabs.io>

--- versions.tf
+++ versions.tf
@@ -10,18 +10,6 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 1.0"
     }
-    # null = {
-    #   source  = "hashicorp/null"
-    #   version = ">= 2.0"
-    # }
-    # local = {
-    #   source  = "hashicorp/local"
-    #   version = ">= 1.3"
-    # }
-    # kubernetes = {
-    #   source  = "hashicorp/kubernetes"
-    #   version = ">= 1.10"
-    # }
     time = {
       source  = "hashicorp/time"
       version = ">= 0.6"

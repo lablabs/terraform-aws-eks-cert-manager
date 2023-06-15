faa9d895824d0729b953ddcf770a952201a3f516
Tomas Balaz <tomasbalaz3012@gmail.com>
2023-06-15 12:01:00 +0200

    feat: align module to the addon-template

--- examples/basic/providers.tf
+++ examples/basic/providers.tf
@@ -14,12 +14,12 @@ provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.this.endpoint
     token                  = data.aws_eks_cluster_auth.this.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
   }
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   token                  = data.aws_eks_cluster_auth.this.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
 }

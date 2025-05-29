8eebc9957f8f3b7d8d43d216c6a92236fb21c721
Attila Sovak <attila.sovak@gmail.com>
2025-05-29 11:49:47 +0200

    feat(cert-manager): change policy to use assume_role via new arn feature and update policy

--- iam.tf
+++ iam.tf
@@ -1,66 +1,48 @@
 locals {
-  irsa_policy_enabled = var.irsa_policy_enabled != null ? var.irsa_policy_enabled : coalesce(var.irsa_assume_role_enabled, false) == false
+  irsa_policy_enabled = var.irsa_policy_enabled != null ? var.irsa_policy_enabled : true
 }
 
 data "aws_iam_policy_document" "this" {
   count = var.enabled && var.irsa_policy == null && local.irsa_policy_enabled ? 1 : 0
-
-  dynamic "statement" {
-    for_each = local.irsa_policy_enabled ? toset(["true"]) : []
-    content {
-      sid    = "ChangeResourceRecordSets"
-      effect = "Allow"
-      actions = [
-        "route53:ChangeResourceRecordSets",
-      ]
-      resources = formatlist(
-        "arn:aws:route53:::hostedzone/%s",
-        var.policy_allowed_zone_ids
-      )
-
+  #https://cert-manager.io/docs/configuration/acme/dns01/route53/
+  #checkov:skip=CKV_AWS_356
+  statement {
+    sid    = "ResourceRecordSets"
+    effect = "Allow"
+    actions = [
+      "route53:ChangeResourceRecordSets",
+      "route53:ListResourceRecordSets",
+    ]
+    resources = formatlist(
+      "arn:aws:route53:::hostedzone/%s",
+      var.policy_allowed_zone_ids
+    )
+    condition {
+      test     = "ForAllValues:StringEquals"
+      variable = "route53:ChangeResourceRecordSetsRecordTypes"
+      values   = ["TXT"]
     }
   }
 
-  dynamic "statement" {
-    for_each = local.irsa_policy_enabled ? toset(["true"]) : []
-    content {
-      sid    = "ListResourceRecordSets"
-      effect = "Allow"
-      actions = [
-        "route53:ListHostedZones",
-        "route53:ListResourceRecordSets",
-        "route53:ListTagsForResource",
-        "route53:ListHostedZonesByName"
-      ]
-      resources = [
-        "*"
-      ]
-    }
+  statement {
+    sid    = "ListResourceRecordSets"
+    effect = "Allow"
+    actions = [
+      "route53:ListHostedZonesByName"
+    ]
+    resources = [
+      "*"
+    ]
   }
 
-  dynamic "statement" {
-    for_each = local.irsa_policy_enabled ? toset(["true"]) : []
-    content {
-      sid    = "GetBatchChangeStatus"
-      effect = "Allow"
-      actions = [
-        "route53:GetChange"
-      ]
-      resources = [
-        "*"
-      ]
-    }
-  }
-
-  dynamic "statement" {
-    for_each = coalesce(var.irsa_assume_role_enabled, false) ? toset(["true"]) : []
-    content {
-      sid    = "AllowAssumeCertManagerRole"
-      effect = "Allow"
-      actions = [
-        "sts:AssumeRole"
-      ]
-      resources = var.irsa_assume_role_arns
-    }
+  statement {
+    sid    = "GetBatchChangeStatus"
+    effect = "Allow"
+    actions = [
+      "route53:GetChange"
+    ]
+    resources = [
+      "arn:aws:route53:::change/*"
+    ]
   }
 }

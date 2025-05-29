1bc7e830da2417f98a01769cb981fadd656269f7
Attila Sovak <attila.sovak@gmail.com>
2025-05-29 09:37:09 +0200

    feat(cert-manager): fix argo_enabled and argo_helm_enabled conditions

--- main.tf
+++ main.tf
@@ -52,9 +52,9 @@ locals {
     name = "cert-manager-cluster-issuer"
 
     helm_chart_version = var.cluster_issuer_helm_chart_version != null ? var.cluster_issuer_helm_chart_version : var.manifest_target_revision
-    helm_repo_url      = var.cluster_issuer_helm_repo_url != null ? var.helm_repo_url : "https://github.com/lablabs/terraform-aws-eks-cert-manager.git"
-    argo_enabled       = var.cluster_issuer_argo_enabled != null ? var.cluster_issuer_argo_enabled : try(module.addon.argo_enabled, true)
-    argo_helm_enabled  = var.cluster_issuer_argo_helm_enabled != null ? var.cluster_issuer_argo_helm_enabled : try(module.addon.argo_helm_enabled, true)
+    helm_repo_url      = var.cluster_issuer_helm_repo_url != null ? var.cluster_issuer_helm_repo_url : "https://github.com/lablabs/terraform-aws-eks-cert-manager.git"
+    argo_enabled       = var.cluster_issuer_argo_enabled != null ? var.cluster_issuer_argo_enabled : try(length(module.addon.kubernetes_application_attributes) > 0, true)
+    argo_helm_enabled  = var.cluster_issuer_argo_helm_enabled != null ? var.cluster_issuer_argo_helm_enabled : try(length(module.addon.kubernetes_application_attributes) > 0, true)
     argo_source_path   = var.cluster_issuer_argo_source_path != null ? var.cluster_issuer_argo_source_path : var.manifest_target_path
   }
 
